41575aaf7fe735dacef93fbb30edb2ca0f80c487
mjzou <mjzou2@illinois.edu>
2019-12-03 03:30:52 -0600

    formatting changes

--- app/src/main/java/com/example/slickkicks/FOptionsActivity.java
+++ app/src/main/java/com/example/slickkicks/FOptionsActivity.java
@@ -2,7 +2,9 @@ package com.example.slickkicks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class FOptionsActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class FOptionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foptions);
+
+        Button next = findViewById(R.id.Next);
+        next.setOnClickListener(unused -> startActivity(new Intent(this, DisplayActivity.class)));
+        finish();
     }
 }
--- app/src/main/java/com/example/slickkicks/OptionsActivity.java
+++ app/src/main/java/com/example/slickkicks/OptionsActivity.java
@@ -2,7 +2,9 @@ package com.example.slickkicks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class OptionsActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class OptionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        Button next = findViewById(R.id.Next);
+        next.setOnClickListener(unused -> startActivity(new Intent(this, DisplayActivity.class)));
+        finish();
     }
 }
